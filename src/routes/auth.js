476e01e3e168f5b1590d4686fae4e1a2d25396f4
mehrdadzeinali <mehrdad.zeinali@epitech.eu>
2023-10-26 20:42:35 +0200

    add id in login response

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -30,19 +30,23 @@ router.post('/register', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await userModel.getUserByEmail(email);
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return res.status(400).json({ error: 'Invalid credentials' });
+    try {
+      const { email, password } = req.body;
+  
+      const user = await userModel.getUserByEmail(email);
+      if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(400).json({ error: 'Invalid credentials' });
+      }
+  
+      const token = jwt.sign({ id: user.id }, 'your_jwt_secret', { expiresIn: '1h' });
+  
+      // Include the user ID along with the token in the response
+      res.json({ id: user.id, token });
+  
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
     }
-
-    const token = jwt.sign({ id: user.id }, 'your_jwt_secret', { expiresIn: '1h' });
-    res.json({ token });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-module.exports = router;
+  });
+  
+  module.exports = router;
+  
