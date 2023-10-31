ba88f964fbef4a7f656b86a50639ab8be9355143
mehrdadzeinali <mehrdad.zeinali@epitech.eu>
2023-10-31 23:14:32 +0100

    src/employee/create don't use the temporary upload file anymore

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = multer(); // Files will be stored in memory as buffer
 const cloudinary = require('cloudinary').v2;
-const rimraf = require('rimraf');
+const employeeModel = require('../models/employee'); // Your employee model
 
 cloudinary.config({ 
   cloud_name: 'dteijjl08', 
@@ -11,43 +11,43 @@ cloudinary.config({
   api_secret: 'Py58yAs5rUiD7ipT04MiChzMT-4' 
 });
 
-const employeeModel = require('../models/employee'); // Your employee model
-
-// Route to create a new employee profile
 router.post('/create', upload.fields([{ name: 'cv', maxCount: 1 }, { name: 'profilePhoto', maxCount: 1 }]), async (req, res) => {
   try {
-    const {
-      firstName,
-      lastName,
-      email,
-      phone,
-      linkedin,
-      category,
-      jobTitle,
-      experience,
-      workType,
-      country,
-      city,
-      situation,
-      languages,
-    } = req.body;
+    const { firstName, lastName, email, phone, linkedin, category, jobTitle, experience, workType, country, city, situation, languages } = req.body;
 
-    // Upload profile photo to Cloudinary
     let profilePhotoURL = null;
+    let cvURL = null;
+    
     if (req.files && req.files.profilePhoto) {
-      const result = await cloudinary.uploader.upload(req.files.profilePhoto[0].path);
-      profilePhotoURL = result.secure_url;
+      await new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream({ resource_type: 'image' }, (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            profilePhotoURL = result.secure_url;
+            resolve();
+          }
+        });
+        const readableStream = require('stream').Readable.from(req.files.profilePhoto[0].buffer);
+        readableStream.pipe(uploadStream);
+      });
     }
 
-    // Upload cv to Cloudinary
-    let cvURL = null;
     if (req.files && req.files.cv) {
-      const result = await cloudinary.uploader.upload(req.files.cv[0].path, { resource_type: 'raw' });
-      cvURL = result.secure_url;
+      await new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream({ resource_type: 'raw' }, (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            cvURL = result.secure_url;
+            resolve();
+          }
+        });
+        const readableStream = require('stream').Readable.from(req.files.cv[0].buffer);
+        readableStream.pipe(uploadStream);
+      });
     }
     
-
-    // Create a new employee document
     const newEmployee = {
       firstName,
       lastName,
@@ -66,18 +66,15 @@ router.post('/create', upload.fields([{ name: 'cv', maxCount: 1 }, { name: 'prof
       profilePhoto: profilePhotoURL,
     };
 
-    const employeeinfo = await employeeModel.createEmployee(newEmployee);
-
-    console.log(employeeinfo)
-
-    rimraf.sync('uploads');
-
-    res.status(201).json({ message: 'Employee profile created successfully', id: employeeinfo.insertId });
+    const employeeInfo = await employeeModel.createEmployee(newEmployee);
+    
+    res.status(201).json({ message: 'Employee profile created successfully', id: employeeInfo.insertId });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
 
 router.get('/:id', async (req, res) => {
   try {
